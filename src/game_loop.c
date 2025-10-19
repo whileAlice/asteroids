f8fee1735c8ed9f819e29e56c277f77a1513d0b0
Lady Alice <althea@disroot.org>
2025-10-19 12:52:36 +0200

    refactor(draw_utils): make drawing functions that take vectors the main variants

--- src/game_loop.c
+++ src/game_loop.c
@@ -2,7 +2,6 @@
 
 #include "game_loop.h"
 #include "draw_utils.h"
-#include "draw_utils_vec.h"
 #include "window_utils.h"
 
 // init
@@ -20,9 +19,9 @@ void
 draw(Image *im)
 {
   clear_image(im, GRAY);
-  draw_triangle_v(im,
-                  (Vector2){ .x = 50.0f,    .y = 100.0f },
-                  (Vector2){ .x = (float)x, .y = (float)y },
-                  (Vector2){ .x = 200,      .y = 180 },
-                  RED, GREEN);
+  draw_triangle(im,
+                (Vector2){ .x = 50.0f,    .y = 100.0f },
+                (Vector2){ .x = (float)x, .y = (float)y },
+                (Vector2){ .x = 200,      .y = 180 },
+                RED, GREEN);
 }
