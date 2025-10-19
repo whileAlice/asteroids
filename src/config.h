130276d8ec18298e3ede2f322662c35b6bbee4ec
Lady Alice <althea@disroot.org>
2025-10-19 12:56:54 +0200

    feat(config): change APP_TITLE to "asteroids"

--- src/config.h
+++ src/config.h
@@ -4,6 +4,6 @@
 #define INITIAL_WINDOW_HEIGHT 576
 #define BUFFER_WIDTH          320
 #define BUFFER_HEIGHT         192
-#define APP_TITLE             "roids"
+#define APP_TITLE             "asteroids"
 
 #define EXIT_KEY              KEY_Q
