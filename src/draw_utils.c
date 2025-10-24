a8574b1cfdbf9e1ebb8c062bbfd7381996c152c8
Lady Alice <althea@disroot.org>
2025-10-24 11:25:05 +0200

    feat(draw_utils): implement drawing non-alpha-blended images

--- src/draw_utils.c
+++ src/draw_utils.c
@@ -8,28 +8,28 @@
 #include "draw_utils.h"
 
 void
-draw_pixel(Image *image, int x, int y, Color3 color)
+draw_pixel(Image *buf, int x, int y, Color3 color)
 {
-  int index = index_from_xy(image, x, y);
+  int index = index_from_xy(buf, x, y);
   if (index == -1) return;
 
-	Color3 *pixels = (Color3 *)image->data;
+	Color3 *pixels = (Color3 *)buf->data;
 	pixels[index] = color;
 }
 
 void
-draw_pixel_unsafe(Image *image, int x, int y, Color3 color)
+draw_pixel_unsafe(Image *buf, int x, int y, Color3 color)
 {
-  size_t index = index_from_xy_unsafe(image, x, y);
+  size_t index = index_from_xy_unsafe(buf, x, y);
 
-	Color3 *pixels = (Color3 *)image->data;
+	Color3 *pixels = (Color3 *)buf->data;
 	pixels[index] = color;
 }
 
 static void
-internal_draw_pixel_a(Image *image, int x, int y, Color color, size_t index)
+internal_draw_pixel_a(Image *buf, int x, int y, Color color, size_t index)
 {
-  Color3 *pixels = (Color3 *)image->data;
+  Color3 *pixels = (Color3 *)buf->data;
   uint8_t a = color.a;
 
   switch (a) {
@@ -48,24 +48,56 @@ internal_draw_pixel_a(Image *image, int x, int y, Color color, size_t index)
 }
 
 void
-draw_pixel_a(Image *image, int x, int y, Color color)
+draw_pixel_a(Image *buf, int x, int y, Color color)
 {
-  int index = index_from_xy(image, x, y);
+  int index = index_from_xy(buf, x, y);
   if (index == -1) return;
 
-  internal_draw_pixel_a(image, x, y, color, index);
+  internal_draw_pixel_a(buf, x, y, color, index);
 }
 
 void
-draw_pixel_a_unsafe(Image *image, int x, int y, Color color)
+draw_pixel_a_unsafe(Image *buf, int x, int y, Color color)
 {
-  size_t index = index_from_xy_unsafe(image, x, y);
+  size_t index = index_from_xy_unsafe(buf, x, y);
 
-  internal_draw_pixel_a(image, x, y, color, index);
+  internal_draw_pixel_a(buf, x, y, color, index);
 }
 
 void
-draw_line_i(Image *image, int start_x, int start_y,
+draw_image(Image *buf, Image *img, int origin_x, int origin_y)
+{
+  const int start_x = CLAMP(origin_x, 0, buf->width  - 1);
+  const int start_y = CLAMP(origin_y, 0, buf->height - 1);
+  const int end_x = CLAMPR(start_x + img->width  - 1, buf->width  - 1);
+  const int end_y = CLAMPR(start_y + img->height - 1, buf->height - 1);
+
+  Color3 *pixels_buf = (Color3 *)buf->data;
+  // TODO: should be Color3 in img data
+  Color  *pixels_img = (Color  *)img->data;
+  int img_y, img_x = 0;
+
+  // TODO: this looks like it could be optimized?
+  for (int buf_x = start_x; buf_x <= end_x; ++buf_x) {
+    img_y = 0;
+
+    for (int buf_y = start_y; buf_y <= end_y; ++buf_y) {
+      size_t index_buf = index_from_xy_unsafe(buf, buf_x, buf_y);
+      size_t index_img = index_from_xy_unsafe(img, img_x, img_y);
+
+      pixels_buf[index_buf].r = pixels_img[index_img].r;
+      pixels_buf[index_buf].g = pixels_img[index_img].g;
+      pixels_buf[index_buf].b = pixels_img[index_img].b;
+
+      img_y++;
+    }
+
+    img_x++;
+  }
+}
+
+void
+draw_line_i(Image *buf, int start_x, int start_y,
             int end_x, int end_y, Color3 color3)
 {
   Color color = c4_from_c3(color3);
@@ -90,20 +122,20 @@ draw_line_i(Image *image, int start_x, int start_y,
   if (is_steep) {
     for (int i = start_x; i <= end_x; ++i) {
       color.a = (uint8_t)(255.f * fractional_part(intersect_y));
-      draw_pixel_a(image, (int)(intersect_y) + 1, i, color);
+      draw_pixel_a(buf, (int)(intersect_y) + 1, i, color);
 
       color.a = (uint8_t)(255.f * (1.0f - fractional_part(intersect_y)));
-      draw_pixel_a(image, (int)(intersect_y), i, color);
+      draw_pixel_a(buf, (int)(intersect_y), i, color);
 
       intersect_y += gradient;
     }
   } else {
     for (int i = start_x; i <= end_x; ++i) {
       color.a = (uint8_t)(255.f * fractional_part(intersect_y));
-      draw_pixel_a(image, i, (int)(intersect_y) + 1, color);
+      draw_pixel_a(buf, i, (int)(intersect_y) + 1, color);
 
       color.a = (uint8_t)(255.f * (1.0f - fractional_part(intersect_y)));
-      draw_pixel_a(image, i, (int)(intersect_y), color);
+      draw_pixel_a(buf, i, (int)(intersect_y), color);
 
       intersect_y += gradient;
     }
@@ -111,39 +143,37 @@ draw_line_i(Image *image, int start_x, int start_y,
 }
 
 void
-draw_line(Image *image, Vector2 start, Vector2 end, Color3 color)
+draw_line(Image *buf, Vector2 start, Vector2 end, Color3 color)
 {
-  draw_line_i(image,
+  draw_line_i(buf,
               (int)roundf(start.x), (int)roundf(start.y),
               (int)roundf(end.x),   (int)roundf(end.y),
               color);
 }
 
 void
-draw_rectangle_fi(Image *image, int origin_x, int origin_y,
+draw_rectangle_fi(Image *buf, int origin_x, int origin_y,
                   int width, int height, Color3 color)
 {
   if (width == 0 || height == 0) return;
 
-  int max_x = image->width  - 1;
-  int max_y = image->height - 1;
-  origin_x  = CLAMP(origin_x, 0, max_x);
-  origin_y  = CLAMP(origin_y, 0, max_y);
-  width     = CLAMP(width,    1, image->width - origin_x);
-  height    = CLAMP(height,   1, image->height - origin_y);
+  const int start_x = CLAMP(origin_x, 0, buf->width  - 1);
+  const int start_y = CLAMP(origin_y, 0, buf->height - 1);
+  const int end_x = CLAMPR(start_x + width  - 1, buf->width  - 1);
+  const int end_y = CLAMPR(start_y + height - 1, buf->height - 1);
 
-  for (int x = origin_x; x < origin_x + width; ++x) {
-    for (int y = origin_y; y < origin_y + height; ++y) {
-      draw_pixel_unsafe(image, x, y, color);
+  for (int x = start_x; x <= end_x; ++x) {
+    for (int y = start_y; y <= end_y; ++y) {
+      draw_pixel_unsafe(buf, x, y, color);
     }
   }
 }
 
 void
-draw_rectangle_f(Image *image, Vector2 origin,
+draw_rectangle_f(Image *buf, Vector2 origin,
                  Vector2 size, Color3 color)
 {
-  draw_rectangle_fi(image,
+  draw_rectangle_fi(buf,
                     (int)roundf(origin.x), (int)roundf(origin.y),
                     (int)roundf(size.x),   (int)roundf(size.y),
                     color);
@@ -151,10 +181,10 @@ draw_rectangle_f(Image *image, Vector2 origin,
 
 
 void
-draw_rectangle_wi(Image *image, int origin_x, int origin_y,
+draw_rectangle_wi(Image *buf, int origin_x, int origin_y,
                   int width, int height, Color3 color)
 {
-  draw_quad_wi(image,
+  draw_quad_wi(buf,
                origin_x,         origin_y,
                origin_x + width, origin_y,
                origin_x + width, origin_y + height,
@@ -163,25 +193,25 @@ draw_rectangle_wi(Image *image, int origin_x, int origin_y,
 }
 
 void
-draw_rectangle_i(Image *image, int origin_x, int origin_y, int width,
+draw_rectangle_i(Image *buf, int origin_x, int origin_y, int width,
                  int height, Color3 border, Color3 fill)
 {
-  draw_rectangle_fi(image, origin_x, origin_y, width, height, fill);
-  draw_rectangle_wi(image, origin_x, origin_y, width, height, border);
+  draw_rectangle_fi(buf, origin_x, origin_y, width, height, fill);
+  draw_rectangle_wi(buf, origin_x, origin_y, width, height, border);
 }
 
 void
-draw_rectangle(Image *image, Vector2 origin, Vector2 size,
+draw_rectangle(Image *buf, Vector2 origin, Vector2 size,
                Color3 border, Color3 fill)
 {
-  draw_rectangle_i(image,
+  draw_rectangle_i(buf,
                    (int)roundf(origin.x), (int)roundf(origin.y),
                    (int)roundf(size.x),   (int)roundf(size.y),
                    border, fill);
 }
 
 void
-draw_triangle_fi(Image *image, int a_x, int a_y, int b_x,
+draw_triangle_fi(Image *buf, int a_x, int a_y, int b_x,
                  int b_y, int c_x, int c_y, Color3 color)
 {
   // TODO: convert to scanline-based approach
@@ -190,10 +220,10 @@ draw_triangle_fi(Image *image, int a_x, int a_y, int b_x,
     SWAP(a_y, b_y);
   }
 
-  const int min_x = CLAMP(MIN(MIN(a_x, b_x), c_x), 0, image->width  - 1);
-  const int min_y = CLAMP(MIN(MIN(a_y, b_y), c_y), 0, image->height - 1);
-  const int max_x = CLAMP(MAX(MAX(a_x, b_x), c_x), 0, image->width  - 1);
-  const int max_y = CLAMP(MAX(MAX(a_y, b_y), c_y), 0, image->height - 1);
+  const int min_x = CLAMP(MIN(MIN(a_x, b_x), c_x), 0, buf->width  - 1);
+  const int min_y = CLAMP(MIN(MIN(a_y, b_y), c_y), 0, buf->height - 1);
+  const int max_x = CLAMP(MAX(MAX(a_x, b_x), c_x), 0, buf->width  - 1);
+  const int max_y = CLAMP(MAX(MAX(a_y, b_y), c_y), 0, buf->height - 1);
 
   for (int x = min_x; x <= max_x; ++x) {
     for (int y = min_y; y <= max_y; ++y) {
@@ -202,34 +232,34 @@ draw_triangle_fi(Image *image, int a_x, int a_y, int b_x,
         is_clockwise(b_x, b_y, c_x, c_y, x, y) &&
         is_clockwise(c_x, c_y, a_x, a_y, x, y)
       ) {
-          draw_pixel_unsafe(image, x, y, color);
+          draw_pixel_unsafe(buf, x, y, color);
         }
     }
   }
 }
 
 void
-draw_triangle_wi(Image *image, int a_x, int a_y, int b_x,
+draw_triangle_wi(Image *buf, int a_x, int a_y, int b_x,
                  int b_y, int c_x, int c_y, Color3 color)
 {
-  draw_line_i(image, a_x, a_y, b_x, b_y, color);
-  draw_line_i(image, b_x, b_y, c_x, c_y, color);
-  draw_line_i(image, c_x, c_y, a_x, a_y, color);
+  draw_line_i(buf, a_x, a_y, b_x, b_y, color);
+  draw_line_i(buf, b_x, b_y, c_x, c_y, color);
+  draw_line_i(buf, c_x, c_y, a_x, a_y, color);
 }
 
 void
-draw_triangle_i(Image *image, int a_x, int a_y, int b_x, int b_y,
+draw_triangle_i(Image *buf, int a_x, int a_y, int b_x, int b_y,
                 int c_x, int c_y, Color3 border, Color3 fill)
 {
-  draw_triangle_fi(image, a_x, a_y, b_x, b_y, c_x, c_y, fill);
-  draw_triangle_wi(image, a_x, a_y, b_x, b_y, c_x, c_y, border);
+  draw_triangle_fi(buf, a_x, a_y, b_x, b_y, c_x, c_y, fill);
+  draw_triangle_wi(buf, a_x, a_y, b_x, b_y, c_x, c_y, border);
 }
 
 void
-draw_triangle(Image *image, Vector2 a, Vector2 b,
+draw_triangle(Image *buf, Vector2 a, Vector2 b,
               Vector2 c, Color3 border, Color3 fill)
 {
-  draw_triangle_i(image,
+  draw_triangle_i(buf,
                   (int)roundf(a.x), (int)roundf(a.y),
                   (int)roundf(b.x), (int)roundf(b.y),
                   (int)roundf(c.x), (int)roundf(c.y),
@@ -237,38 +267,38 @@ draw_triangle(Image *image, Vector2 a, Vector2 b,
 }
 
 void
-draw_quad_wi(Image *image, int a_x, int a_y, int b_x, int b_y,
+draw_quad_wi(Image *buf, int a_x, int a_y, int b_x, int b_y,
              int c_x, int c_y, int d_x, int d_y, Color3 color)
 {
-  draw_line_i(image, a_x, a_y, b_x, b_y, color);
-  draw_line_i(image, b_x, b_y, c_x, c_y, color);
-  draw_line_i(image, c_x, c_y, d_x, d_y, color);
-  draw_line_i(image, d_x, d_y, a_x, a_y, color);
+  draw_line_i(buf, a_x, a_y, b_x, b_y, color);
+  draw_line_i(buf, b_x, b_y, c_x, c_y, color);
+  draw_line_i(buf, c_x, c_y, d_x, d_y, color);
+  draw_line_i(buf, d_x, d_y, a_x, a_y, color);
 }
 
 void
-clear_image(Image *image, Color3 color)
+clear_image(Image *img, Color3 color)
 {
-  Color3 *addr = image->data;
-  size_t count = image->width * image->height;
+  Color3 *addr = img->data;
+  size_t count = img->width * img->height;
 
   while (count--) *addr++ = color;
 }
 
 int
-index_from_xy(Image *image, int x, int y)
+index_from_xy(Image *img, int x, int y)
 {
-  const int index = image->width * y + x;
-  const int max_index = image->width * image->height - 1;
+  const int index = img->width * y + x;
+  const int max_index = img->width * img->height - 1;
   assert(max_index >= 0);
 
 	return index >= 0 && index <= max_index ? index : -1;
 }
 
 size_t
-index_from_xy_unsafe(Image *image, int x, int y)
+index_from_xy_unsafe(Image *img, int x, int y)
 {
-  return image->width * y + x;
+  return img->width * y + x;
 }
 
 Color3
--- src/game_loop.h
+++ src/game_loop.h
@@ -2,5 +2,7 @@
 
 #include <raylib.h>
 
+void game_init();
 void update(float dt);
 void draw(Image *image);
+void game_deinit();
