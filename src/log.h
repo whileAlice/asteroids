a455c2591af0ee0defd57d20c3c7b9332f853aeb
Lady Alice <althea@disroot.org>
2025-10-26 17:10:37 +0100

    feat(log): implement osd support

--- src/log.h
+++ src/log.h
@@ -7,4 +7,6 @@
 void init_log_buffers(FixedFont* font);
 void add_line_to_console_log(const char* line);
 void show_console_log(Image* buf);
+void print_to_osd(const char* text, size_t row, size_t col);
+void show_osd(Image* buf);
 void deinit_log_buffers();
