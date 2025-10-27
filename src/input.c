7f573128512ba4fc25ea32c5d70f1751c1ce912a
Lady Alice <althea@disroot.org>
2025-10-27 21:33:55 +0100

    feat(log): handle variadic string formatting for osd calls

--- src/input.c
+++ src/input.c
@@ -8,11 +8,11 @@ extern Context g_ctx;
 void
 update_input()
 {
-  g_ctx.input.system.show_osd     = IsKeyPressed(KEY_GRAVE) && !IsKeyDown(KEY_RIGHT_SHIFT);
-  g_ctx.input.system.show_console = IsKeyPressed(KEY_GRAVE) && IsKeyDown(KEY_RIGHT_SHIFT);
+  g_ctx.input.show_osd     = IsKeyPressed(KEY_GRAVE) && !IsKeyDown(KEY_RIGHT_SHIFT);
+  g_ctx.input.show_console = IsKeyPressed(KEY_GRAVE) && IsKeyDown(KEY_RIGHT_SHIFT);
 
-  g_ctx.input.player.accelerate   = IsKeyPressed(KEY_W);
-  g_ctx.input.player.decelerate   = IsKeyPressed(KEY_S);
-  g_ctx.input.player.turn_left    = IsKeyPressed(KEY_A);
-  g_ctx.input.player.turn_right   = IsKeyPressed(KEY_D);
+  g_ctx.input.accelerate   = IsKeyPressed(KEY_W);
+  g_ctx.input.decelerate   = IsKeyPressed(KEY_S);
+  g_ctx.input.turn_left    = IsKeyPressed(KEY_A);
+  g_ctx.input.turn_right   = IsKeyPressed(KEY_D);
 }
